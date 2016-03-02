2caf70d335830cd76a46f71e67b01bb24496740c
323567263 <323567263@C-4JNJVR1.tdsb.on.ca>
2016-03-02 14:44:53 -0500

    Created Hello World.

---
+++ ICS3UP/src/firth/HelloWorld.java
@@ -0,0 +1,27 @@
+package firth;
+/* Title
+ * Description
+ * Name
+ * Date
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		
+		int x=0;
+		
+
+		while(x<50)
+		{
+			x++;
+		
+		System.out.println("x");
+		
+		}
+		}
+
+	}
+
+
