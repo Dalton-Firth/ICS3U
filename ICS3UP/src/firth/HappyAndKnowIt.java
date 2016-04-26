b3dcdec43ddf4c2c152e913f7debd0aa1599a735
323567263 <323567263@C-4JNJVR1.tdsb.on.ca>
2016-04-26 14:29:18 -0400

    Final Version

---
+++ ICS3UP/src/firth/HappyAndKnowIt.java
@@ -0,0 +1,32 @@
+package firth;
+/*Dalton Firth
+ * Happy and you Know it
+ * Created: 2016/04/26
+ * Outputs the lyrics of a song
+ */
+
+
+public class HappyAndKnowIt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String[] actions = new String[]{" clap your hands."," stomp your feet."," shout Hurray!"," do all three."," scratch your side."," jump up and down."," ooh-ooh.","do all three.", " lie down.", " go to sleep."};
+		
+		for(int i=0;i<10;i++)
+		{
+			System.out.println("If you're happy and you know it"+actions[i]);
+			System.out.println("If you're happy and you know it"+actions[i]);
+			System.out.println("If you're happy and you know it and you really want to show it.");
+			System.out.println("If you're happy and you know it"+actions[i]);
+			System.out.println("");
+		}
+		
+		
+		
+		
+		
+		
+	}
+
+}
