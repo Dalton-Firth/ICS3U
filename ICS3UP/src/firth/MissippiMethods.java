eff85bbbcf2cb01f3a1e535818de3c838c180584
323567263 <323567263@C-4JNJVR1.tdsb.on.ca>
2016-05-17 15:03:56 -0400

    Final version

---
+++ ICS3UP/src/firth/MissippiMethods.java
@@ -0,0 +1,91 @@
+package firth;
+
+/*Dalton Firth
+ * Mississippi
+ * Created: 2016/05/17
+ * Prints a big Mississippi
+ */
+public class MissippiMethods {
+	
+	
+	/*
+	 * This is the main method it calls the other methods in the program
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//calls methods to print a message
+		m();
+		i();
+		s();
+		s();
+		i();
+		s();
+		s();
+		i();
+		p();
+		p();
+		i();
+		
+	}
+	
+	/*
+	 * This prints a big M made of smaller M's
+	 */
+	public static void m()
+	{
+		System.out.println("");
+		System.out.println("M       M");
+		System.out.println("M M   M M");
+		System.out.println("M  M M  M");
+		System.out.println("M   M   M");
+		System.out.println("M       M");
+		System.out.println("M       M");
+		System.out.println("M       M");
+		
+	}
+	/*
+	 * This prints a big I made of smaller I's
+	 */
+	public static void i()
+	{
+		System.out.println("");
+		System.out.println("IIIII");
+		System.out.println("  I  ");
+		System.out.println("  I  ");
+		System.out.println("  I  ");
+		System.out.println("  I  ");
+		System.out.println("  I  ");
+		System.out.println("IIIII");
+	}
+	/*
+	 * This prints a big S made of smaller S's
+	 */
+	public static void s()
+	{
+		System.out.println("");
+		System.out.println(" SSSSS ");
+		System.out.println("S     S");
+		System.out.println("S      ");
+		System.out.println(" SSSSS ");
+		System.out.println("      S");
+		System.out.println("S     S");
+		System.out.println(" SSSSS ");
+	}
+	/*
+	 * This prints a big P made of smaller P's
+	 */
+	public static void p()
+	{
+		System.out.println("");
+		System.out.println(" PPPPP ");
+		System.out.println("P     P");
+		System.out.println("P     P");
+		System.out.println("PPPPPP ");
+		System.out.println("P      ");
+		System.out.println("P      ");
+		System.out.println("P      ");
+	}
+	
+
+}
